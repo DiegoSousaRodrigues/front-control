fced2422928348935362d979523285e2a4bf8cb0
DiegoSousaRodrigues <diego.srodrigues2002@gmail.com>
2025-10-02 21:48:18 -0300

    Update links to use product editing routes in TableProduct and Edit components

--- pages/product/edit/[id].tsx
+++ pages/product/edit/[id].tsx
@@ -1,10 +1,10 @@
-import FormClient from '@/components/FormClient'
-import { findById } from '@/services/client'
+import FormProduct from '@/components/FormProduct'
+import { findById } from '@/services/product'
 import { ClientDetails } from '@/types/client'
 import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 
 export default function Edit(props: ClientDetails) {
-  return <FormClient props={props} type='edit' />
+  return <FormProduct props={props} type='edit' />
 }
 
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
--- src/components/ListScreen/Tables/Product.tsx
+++ src/components/ListScreen/Tables/Product.tsx
@@ -33,7 +33,7 @@ export function TableProduct({
             <TableRowHeaderCell>{price}</TableRowHeaderCell>
             <TableCell>
               <div className='flex gap-2'>
-                <Link href={`/client/edit/${id}`}>
+                <Link href={`/product/edit/${id}`}>
                   <MdOutlineModeEditOutline size={24} />
                 </Link>
                 <Button onClick={() => handleDisableOrActiveProduct(id, active)}>
