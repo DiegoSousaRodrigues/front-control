6b93f3912936415d41b2598b613d185d79e6d673
DiegoSousaRodrigues <diego.srodrigues2002@gmail.com>
2025-11-24 19:46:57 -0300

    feat(LoginScreen): implement login form with validation and submission handling; add next-auth dependency

--- src/components/LoginScreen/LoginScreen.tsx
+++ src/components/LoginScreen/LoginScreen.tsx
@@ -1,14 +1,23 @@
+import { useForm } from 'react-hook-form'
 import { Input } from '../lib/Input/Input'
+import { LoginForm } from './LoginScreen.types'
 
 export function LoginScreen() {
+  const { register, handleSubmit } = useForm<LoginForm>()
+
+  function onSubmit(data: LoginForm) {
+    console.log(data)
+  }
+
   return (
     <div className='flex w-full h-full bg-gray-100 items-center justify-center'>
-      <div className='flex w-[400px] h-[350px] bg-white px-6 py-4 '>
-        <div className='w-full flex flex-col gap-4'>
-          <Input label='login' />
-          <Input label='senha' />
-          
-        </div>
+      <div className='flex w-[400px] bg-white px-6 py-6 rounded-lg'>
+        <form onSubmit={handleSubmit(onSubmit)} className='w-full flex flex-col gap-4'>
+          <h1 className='text-2xl text-center font-bold'>Login</h1>
+          <Input label='login' {...register('login', { required: true })} />
+          <Input label='senha' {...register('password', { required: true })} />
+          <button className='py-4 rounded-lg bg-primary text-white font-bold'>Entrar</button>
+        </form>
       </div>
     </div>
   )
